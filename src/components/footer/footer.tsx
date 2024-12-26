a58ae9391fa01c0269b688f22f8faba7e90537a7
saadcraft <39559368+saadcraft@users.noreply.github.com>
2024-12-27 00:11:37 +0100

    update for image

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <div className='max-w-7xl mx-auto pt-16 pb-5 px-5'>
             <div className='flex items-center md:items-start flex-col md:flex-row justify-between gap-y-14 text-center md:text-left'>
                 <div className='w-full max-w-80 text-white'>
-                    <Image width={100} height={100}  src="./TAWSILSTAR5.png" alt="Perfect-room Logo" className='py-4 w-40'/>
+                    <Image width={100} height={100}  src="/TAWSILSTAR5.png" alt="Perfect-room Logo" className='py-4 w-40'/>
                 </div>
                 <div className='grid grid-cols-2 md:gap-x-20 md:gap-y-20 gap-x-6 gap-y-6'>
                     <div className='flex flex-col'>
